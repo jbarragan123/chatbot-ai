893ce242e85e768afec0850b41535ad88032fcbc
jbarragan123 <juanblow3@gmail.com>
2025-07-26 13:24:53 -0500

    add: swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Product Assistant API')
+    .setDescription('API for querying and converting product info using OpenAI')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
